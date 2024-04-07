4087d5f5499a039c1664636a83e23eaf39bc043c
Rtst007 <744915796@qq.com>
2024-04-07 09:31:24 +0800

    333

---
+++ 03-Tensorflow2搭建优化神经网络-北京大学/a-readme.txt
@@ -0,0 +1,9 @@
+
+
+bilibili homepage
+
+https://space.bilibili.com/369254015
+
+视频链接
+
+https://www.bilibili.com/video/BV1B7411L7Qt/?p=3&spm_id_from=pageDriver&vd_source=b0fa6bce2d8bcdeb62f557286750afd0
---
+++ 03-Tensorflow2搭建优化神经网络-北京大学/test.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# 说明：
+#
+# os.environ["TF_CPP_MIN_LOG_LEVEL"] = '1' # 默认，显示所有信息
+# os.environ["TF_CPP_MIN_LOG_LEVEL"] = '2' # 只显示 warning 和 Error
+# os.environ["TF_CPP_MIN_LOG_LEVEL"] = '3' # 只显示 Error
+# 如果需要对CPU进行优化，可以访问下面的github，重新编译tensorflow源码以兼容AVX
+# https://github.com/lakshayg/tensorflow-build
+
+
+# tensorflow_version = tf.__version__
+# # gpu_available = tf.test.is_gpu_available()
+#
+# gpu_available = tf.config.list_physical_devices('GPU')
+#
+#
+# print("tensorflow version:", tensorflow_version, "\tGPU available:", gpu_available)
+#
+# a = tf.constant([1.0, 2.0], name="a")
+# b = tf.constant([1.0, 2.0], name="b")
+# result = tf.add(a, b, name="add")
+# print(result)
+
+
+classes = 4
+labels = tf.constant([1,4,2]) # 输入的元素值 4 超出 depth-1
+output = tf.one_hot(labels,depth=classes)
+print(output)
+
+
+
